f5b8b7a08bb2cb5189bf96f760723a97dd12a029
Старый_Варик <seva200398@gmail.com>
2023-04-22 10:17:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram import Bot, types, utils
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.types import InputTextMessageContent, InlineQueryResultArticle, input_message_content
-from aiogram.client.session.aiohttp import AiohttpSession
+#from aiogram.client.session.aiohttp import AiohttpSession
 
 
 from youtube_search import YoutubeSearch
